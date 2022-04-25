4ca5f9da89ac8e787d5739167217bb3cf61499bd
Kaoré <kaore@openagenda.com>
2022-04-25 09:08:23 +0200

    added image to list items

--- components/EventItem.js
+++ components/EventItem.js
@@ -1,7 +1,35 @@
+import Image from 'next/image';
+
 const label = (obj, lang) => obj?.[lang] ?? obj[Object.keys(obj).shift()];
+const imageInfo = (event, type) => {
+  const {
+    base
+  } = event.image;
+
+  const {
+    size,
+    filename
+  } = event.image.variants.find(v => v.type === type);
+
+  return {
+    src: base + filename,
+    width: size.width,
+    height: size.height
+  }
+}
 
 function EventItem({ event, locale }) {
+  const image = event.image ? imageInfo(event, 'thumbnail') : null;
   return <div>
+    {image ? (
+      <Image
+        className="img-thumbnail"
+        alt={label(event.title, locale)}
+        src={image.src}
+        width={image.width}
+        height={image.height}
+      />
+    ) : null}
     <strong>{label(event.title, locale)}</strong>
     <div>
       <span>{label(event.dateRange, locale)}</span>
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['cibul.s3.amazonaws.com'],
+  },
+};
